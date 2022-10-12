276da3bb914f6fafe3ffdc9a05197b3ec7559261
Sofikul Mallick <sofikul.projects@gmail.com>
2022-10-12 18:09:14 +0530

    Feature cloud database support added

--- db-connect.js
+++ db-connect.js
@@ -4,17 +4,9 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const dbCred = {
-  user: process.env.POSTGRESQL_USER,
-  host: process.env.POSTGRESQL_HOST,
-  database: process.env.POSTGRESQL_DATABASE,
-  password: process.env.POSTGRESQL_PASSWORD,
-  port: 5433,
-};
-
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on("connect", () => {
